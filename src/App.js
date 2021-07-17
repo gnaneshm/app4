191f59f27affec86efad8c19421f3f763b33c8e4
Gnanesh M <gnaneshm@Gnaneshs-MacBook-Pro.local>
2021-07-17 11:30:50 +0530

    submodules added

--- src/App.js
+++ src/App.js
@@ -1,24 +1,13 @@
-import logo from './logo.svg';
-import './App.css';
+import Button from './components/button'
+import Dropdown from './components/dropdown'
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+    <h1>App4 components</h1>
+    <Button color="blue"/>
+    <Dropdown color="blue"/>
+    </>
   );
 }
 
