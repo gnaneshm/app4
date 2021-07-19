900321e998f5bd8ed98c20447ba303d671c2a6b3
Gnanesh M <gnaneshm@Gnaneshs-MacBook-Pro.local>
2021-07-19 19:37:46 +0530

    new branch 2222 created

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import Dropdown from './components/dropdown'
 function App() {
   return (
     <>
-    <h1>App4 components</h1>
+    <h1>App4 components 2222 branch created</h1>
     <Button color="blue"/>
     <Dropdown color="blue"/>
     </>
